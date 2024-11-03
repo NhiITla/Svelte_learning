b2d12267b0f9dd48d03ea5bb30b242a2a0121ae7
APAC\HYN4HC <Nhi.NguyenThiHuyen@vn.bosch.com>
2024-11-03 20:59:51 +0700

    Added component

--- Code/src/App.svelte
+++ Code/src/App.svelte
@@ -1,13 +1,34 @@
 <script>
 	export let name;
 	import Header from "./UI/Header.svelte";
+	import MeetupsItem from "./Meetups/MeetupsItem.svelte";
+
+	const meetups=[
+		{
+			id:"m1",
+			title:"Coding Bootcamp",
+			sub:"Coding Bootcamp",
+			des:"in this meetup, we will have some others thing",
+			imageUrl:"https://store.google.com/regionpicker?hl=en-US",
+			email:"email1@gmail.com"
+		},
+		{
+			id:"m2",
+			title:"Coding Bootcamp",
+			sub:"Coding Bootcamp",
+			des:"in this meetup, we will have some others thing",
+			imageUrl:"https://store.google.com/regionpicker?hl=en-US",
+			email:"email2@gmail.com"
+		}
+
+	]
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
 </style>
 
 <Header/>
-<h1>Hello 1 {name}!</h1>
+{#each meetups as meetup }
+	<MeetupsItem/>
+	
+{/each}
---
+++ Code/src/Meetups/MeetupsItem.svelte
@@ -0,0 +1,15 @@
+<article>
+    <header>
+        <h1>TITLE</h1>
+        <h2>SUB</h2>
+    </header>
+    <div class="image">
+        <img src="" alt="">
+    </div>
+    <div class="content">
+        <p></p>
+    </div>
+    <footer>
+        <button>SHOW DETAILS</button>
+    </footer>
+</article>
