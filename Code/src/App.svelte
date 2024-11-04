7e534553ea1eab3c625e950ed629f30abe55afbb
APAC\HYN4HC <Nhi.NguyenThiHuyen@vn.bosch.com>
2024-11-04 16:05:21 +0700

    Passing data to components

--- Code/src/App.svelte
+++ Code/src/App.svelte
@@ -1,5 +1,4 @@
 <script>
-	export let name;
 	import Header from "./UI/Header.svelte";
 	import MeetupsItem from "./Meetups/MeetupsItem.svelte";
 
@@ -9,7 +8,7 @@
 			title:"Coding Bootcamp",
 			sub:"Coding Bootcamp",
 			des:"in this meetup, we will have some others thing",
-			imageUrl:"https://store.google.com/regionpicker?hl=en-US",
+			imageUrl:"https://kidlingoo.com/wp-content/uploads/flowers_name_in_english-980x510.jpg.webp",
 			email:"email1@gmail.com"
 		},
 		{
@@ -17,7 +16,7 @@
 			title:"Coding Bootcamp",
 			sub:"Coding Bootcamp",
 			des:"in this meetup, we will have some others thing",
-			imageUrl:"https://store.google.com/regionpicker?hl=en-US",
+			imageUrl:"https://dictionary.cambridge.org/images/thumb/flower_noun_002_14403.jpg?version=6.0.36",
 			email:"email2@gmail.com"
 		}
 
@@ -29,6 +28,10 @@
 
 <Header/>
 {#each meetups as meetup }
-	<MeetupsItem/>
+	<MeetupsItem 
+	title={meetup.title} 
+	sub={meetup.sub} 
+	des={meetup.des} 
+	imageUrl={meetup.imageUrl}/>
 	
 {/each}
--- Code/src/Meetups/MeetupsItem.svelte
+++ Code/src/Meetups/MeetupsItem.svelte
@@ -1,15 +1,27 @@
+<script>
+    export let title; //export set data from our site
+    export let sub;
+    export let des;
+    export let imageUrl;
+
+
+</script>
+<style>
+
+</style>
 <article>
     <header>
-        <h1>TITLE</h1>
-        <h2>SUB</h2>
+        <h1>{title}</h1>
+        <h2>{sub}</h2>
     </header>
     <div class="image">
-        <img src="" alt="">
+        <img src="{imageUrl}" alt="{title}">
     </div>
     <div class="content">
         <p></p>
     </div>
     <footer>
         <button>SHOW DETAILS</button>
+        <button>Favorite</button>
     </footer>
 </article>
