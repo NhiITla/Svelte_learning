730a7cfdde05651f471a1d14d64fc727ca0a5faf
APAC\HYN4HC <Nhi.NguyenThiHuyen@vn.bosch.com>
2024-11-06 14:05:00 +0700

    Added meetups via a form

--- Code/src/App.svelte
+++ Code/src/App.svelte
@@ -3,7 +3,12 @@ import Header from "./UI/Header.svelte";
 // import MeetupsItem from "./Meetups/MeetupsItem.svelte";
 import MeetupGrid from "./Meetups/MeetupGrid.svelte";
 
-const meetups = [{
+let title = '';
+let sub = '';
+let des = '';
+let imageUrl = '';
+
+let meetups = [{
         id: "m1",
         title: "Coding Bootcamp",
         sub: "Coding Bootcamp",
@@ -21,6 +26,19 @@ const meetups = [{
     }
 
 ]
+
+function addMeetup() {
+    let newMeetup={
+        id: Math.random().toString(),
+        title: title,
+        sub: sub,
+        des: des,
+        imageUrl: imageUrl
+    };
+
+    // meetups.push(newMeetup) //DOES NOT WORK -> not update the dom , not work for svelte -> use js
+    meetups=[...meetups, newMeetup];
+}
 </script>
 
 <style>
@@ -31,5 +49,24 @@ main {
 
 <Header/>
     <main>
-        <MeetupGrid meetups={meetups}/>
-            </main>
+        <form on:submit|preventDefault="{addMeetup}">
+            <div class="form-control">
+                <label for="title">Title</label>
+                <input type="text" id="title" bind:value={title}/>
+            </div>
+            <div class="form-control">
+                <label for="sub">Sub</label>
+                <input type="text" id="sub" bind:value={sub}/>
+            </div>
+            <div class="form-control">
+                <label for="imageUrl">Image URL</label>
+                <input type="text" id="imageUrl" bind:value={imageUrl}/>
+            </div>
+            <div class="form-control">
+                <label for="des">Description</label>
+                <textarea rows="3" id="des" bind:value={des}/>
+                    </div>
+                    <button>Submit</button>
+                    </form>
+                    <MeetupGrid meetups={meetups}/>
+                        </main>
