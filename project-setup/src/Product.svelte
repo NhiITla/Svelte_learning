4057242c509a259b4dba5b5632ac9b1b4560b2f9
APAC\HYN4HC <Nhi.NguyenThiHuyen@vn.bosch.com>
2024-11-10 18:49:10 +0700

    Event forwarding added

---
+++ project-setup/src/Product.svelte
@@ -0,0 +1,8 @@
+<script>
+export let productTitle;
+</script>
+<article>
+    <h1>{productTitle}</h1>
+    <button on:click>Add to Cart because on:click so event will handle on App component</button>
+    <button>Delete</button>
+</article>
