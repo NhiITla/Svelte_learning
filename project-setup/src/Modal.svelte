8eb20e7c3ce9f854aa1cbf9f1e26f06fb147f05d
APAC\HYN4HC <Nhi.NguyenThiHuyen@vn.bosch.com>
2024-11-13 16:32:29 +0700

    Create and Update lifecycle Hook

--- project-setup/src/Modal.svelte
+++ project-setup/src/Modal.svelte
@@ -1,10 +1,39 @@
 <script>
 import {
-    createEventDispatcher
-} from 'svelte'
+    createEventDispatcher,
+    onDestroy,
+    onMount,
+    beforeUpdate,
+    afterUpdate
+} from 'svelte';
+
 export let content;
 const dispatch = createEventDispatcher();
-let agreed=false;
+let agreed = false;
+let autoscroll = false;
+
+onMount(() => {
+    console.log("onMount");
+});
+
+onDestroy(() => {
+    console.log("onDestroy");
+});
+
+beforeUpdate(() => {
+    console.log("beforeUpdate");
+    autoscroll = agreed;
+});
+
+afterUpdate(() => {
+    console.log("afterUpdate");
+    if(autoscroll){
+        const modal = document.querySelector('.modal');
+        modal.scrollTo(0, modal.scrollHeight)
+    }
+});
+
+console.log("Script executed!")
 </script>
 
 <style>
@@ -24,7 +53,7 @@ let agreed=false;
     top: 10vh;
     left: 10%;
     width: 80%;
-    max-height: 80vh;
+    max-height: 15vh;
     background: white;
     border-radius: 5px;
     z-index: 100;
