6ea262421cef779f5c5b53f44cb632faaed30e38
APAC\HYN4HC <Nhi.NguyenThiHuyen@vn.bosch.com>
2024-11-15 18:21:05 +0700

    custom binding event

--- starting-project/src/App.svelte
+++ starting-project/src/App.svelte
@@ -1,7 +1,11 @@
 <script>
-let val = "MAX";
+	import CustomInput from "./CustomInput.svelte";
+	import Toggle from "./Toggle.svelte";
+	let val = "MAX";
+	let selectedOption=1;
 
 $: console.log(val);
+$: console.log(selectedOption);
 
 function setValue(event) {
     val = event.target.value
@@ -9,4 +13,6 @@ function setValue(event) {
 </script>
 
 <!-- <input type="text" value={val} on:input={setValue}> -->
-<input type="text" bind:value={val}>
+<!-- <input type="text" bind:value={val}> -->
+ <CustomInput bind:val={val}/>
+<Toggle bind:chosenOption={selectedOption}></Toggle>
