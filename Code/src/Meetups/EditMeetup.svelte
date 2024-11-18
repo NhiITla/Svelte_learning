e1c06ff0bc45a7fd3e761e92c1042bf8179c7b05
APAC\HYN4HC <Nhi.NguyenThiHuyen@vn.bosch.com>
2024-11-18 11:30:29 +0700

    validation in form

--- Code/src/Meetups/EditMeetup.svelte
+++ Code/src/Meetups/EditMeetup.svelte
@@ -1,69 +1,119 @@
 <script>
-import TextInput from "../UI/TextInput.svelte";
-import Button from "../UI/Button.svelte";
-import {
-    createEventDispatcher
-} from 'svelte';
+    import { createEventDispatcher } from "svelte";
+    import TextInput from "../UI/TextInput.svelte";
+    import Button from "../UI/Button.svelte";
+    import Modal from "../UI/Modal.svelte";
+    import { isEmpty, isValidEmail } from "../helpers/validation.js";
 
-import Modal from "../UI/Modal.svelte";
-let title = '';
-let sub = '';
-let des = '';
-let imageUrl = '';
+    let title = "";
+    let titleValid = false;
+    let subtitle = "";
+    let subtitleValid = false;
+    let address = "";
+    let addressValid = false;
+    let email = "";
+    let emailValid = false;
+    let description = "";
+    let descriptionValid = false;
+    let imageUrl = "";
+    let imageUrlValid = false;
+    let formIsValid = false;
 
-const dispatch = createEventDispatcher();
+    const dispatch = createEventDispatcher();
 
-function submitForm() {
-    dispatch('save', {
-        title: title,
-        sub: sub,
-        imageUrl: imageUrl
-    })
-}
+    $: titleValid = !isEmpty(title);
+    $: subtitleValid = !isEmpty(subtitle);
+    $: addressValid = !isEmpty(address);
+    $: descriptionValid = !isEmpty(description);
+    $: imageUrlValid = !isEmpty(imageUrl);
+    $: emailValid = isValidEmail(email);
+    $: formIsValid =
+        titleValid &&
+        subtitleValid &&
+        addressValid &&
+        descriptionValid &&
+        imageUrlValid &&
+        emailValid;
 
-function cancel(){
-    dispatch('cancel');
-}
+    function submitForm() {
+        dispatch("save", {
+            title: title,
+            subtitle: subtitle,
+            address: address,
+            email: email,
+            description: description,
+            imageUrl: imageUrl,
+        });
+    }
+
+    function cancel() {
+        dispatch("cancel");
+    }
 </script>
 
-<style>
-form {
-    width: 30rem;
-    max-width: 90%;
-    margin: auto;
-}
-</style>
-
-<Modal title="Edit Meetup data" on:cancel>
+<Modal title="Edit Meetup Data" on:cancel>
     <form on:submit|preventDefault={submitForm}>
         <TextInput
             id="title"
             label="Title"
-            type = "Text"
+            valid={titleValid}
+            validityMessage="Please enter a valid title."
             value={title}
-            on:input={event =>(title = event.target.value)} />
+            on:input={(event) => (title = event.target.value)}
+        />
         <TextInput
-            id="sub"
-            label="Sub"
-            type = "Text"
-            value={sub}
-            on:input={event =>(sub = event.target.value)} />
+            id="subtitle"
+            label="Subtitle"
+            valid={subtitleValid}
+            validityMessage="Please enter a valid subtitle."
+            value={subtitle}
+            on:input={(event) => (subtitle = event.target.value)}
+        />
+        <TextInput
+            id="address"
+            label="Address"
+            valid={addressValid}
+            validityMessage="Please enter a valid address."
+            value={address}
+            on:input={(event) => (address = event.target.value)}
+        />
         <TextInput
             id="imageUrl"
-            label="Image Url"
-            type = "Text"
+            label="Image URL"
+            valid={imageUrlValid}
+            validityMessage="Please enter a valid image url."
             value={imageUrl}
-            on:input={event =>(imageUrl = event.target.value)} />
+            on:input={(event) => (imageUrl = event.target.value)}
+        />
         <TextInput
-            id="des"
+            id="email"
+            label="E-Mail"
+            type="email"
+            valid={emailValid}
+            validityMessage="Please enter a valid email address."
+            value={email}
+            on:input={(event) => (email = event.target.value)}
+        />
+        <TextInput
+            id="description"
             label="Description"
-            controlType = "textarea"
-            value={des}
-            on:input={event =>(des = event.target.value)} />
-
+            controlType="textarea"
+            valid={descriptionValid}
+            validityMessage="Please enter a valid description."
+            value={description}
+            on:input={(event) => (description = event.target.value)}
+        />
     </form>
     <div slot="footer">
-        <Button type="button" on:click={submitForm} >Save</Button>
-        <Button type="button" on:click={cancel} >Cancel</Button>
+        <Button type="button" mode="outline" on:click={cancel}>Cancel</Button>
+        <Button type="button" on:click={submitForm} disabled={!formIsValid}
+            >Save</Button
+        >
     </div>
 </Modal>
+
+<style>
+    form {
+        width: 100%;
+    }
+</style>
--- Code/src/UI/TextInput.svelte
+++ Code/src/UI/TextInput.svelte
@@ -1,52 +1,80 @@
 <script>
-export let controlType;
-export let id;
-export let label;
-export let rows;
-export let value;
-export let type;
+    export let controlType = null;
+    export let id;
+    export let label;
+    export let rows = null;
+    export let value;
+    export let type = "text";
+    export let valid = true;
+    export let validityMessage = "";
+    let touched = false;
 </script>
 
-<style>
-input,
-textarea {
-    display: block;
-    width: 100%;
-    font: inherit;
-    border: none;
-    border-bottom: 2px solid #ccc;
-    border-radius: 3px 3px 0 0;
-    background: white;
-    padding: 0.15rem 0.25rem;
-    transition: border-color 0.1s ease-out;
-}
-
-input:focus,
-textarea:focus {
-    border-color: #e40763;
-    outline: none;
-}
-
-label {
-    display: block;
-    margin-bottom: 0.5rem;
-    width: 100%;
-}
-
-.form-control {
-    padding: 0.5rem 0;
-    width: 100%;
-    margin: 0.25rem 0;
-}
-</style>
-
 <div class="form-control">
-    <label for="{id}">{label}</label>
-    {#if  controlType==="textarea"}
-    <textarea rows="{rows}" id="des" value={value} on:input/>
-        {:else}
-        <input type="{type}" id="{id}" value={value} on:input/>
+    <label for={id}>{label}</label>
+    {#if controlType === "textarea"}
+        <textarea
+            class:invalid={!valid && touched}
+            {rows}
+            id="des"
+            {value}
+            on:input
+            on:blur={() => (touched = true)}
+        />
+    {:else}
+        <input
+            class:invalid={!valid && touched}
+            {type}
+            {id}
+            {value}
+            on:input
+            on:blur={() => (touched = true)}
+        />
+    {/if}
+    {#if validityMessage && !valid && touched}
+        <p class="error-message">{validityMessage}</p>
+    {/if}
+</div>
 
-        {/if}
+<style>
+    input,
+    textarea {
+        display: block;
+        width: 100%;
+        font: inherit;
+        border: none;
+        border-bottom: 2px solid #ccc;
+        border-radius: 3px 3px 0 0;
+        background: white;
+        padding: 0.15rem 0.25rem;
+        transition: border-color 0.1s ease-out;
+    }
 
-        </div>
+    input:focus,
+    textarea:focus {
+        border-color: #e40763;
+        outline: none;
+    }
+
+    label {
+        display: block;
+        margin-bottom: 0.5rem;
+        width: 100%;
+    }
+
+    .form-control {
+        padding: 0.5rem 0;
+        width: 100%;
+        margin: 0.25rem 0;
+    }
+
+    .invalid {
+        border-color: red;
+        background: #fde3e3;
+    }
+
+    .error-message {
+        color: red;
+        margin: 0.25rem 0;
+    }
+</style>
